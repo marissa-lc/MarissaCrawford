4cdd7b24ef25311fb78586c597b6b3ce245dac90
RIssa <rissa_cford@yahoo.com>
2020-04-06 01:06:02 -0700

    added components folder and js files. Started links for pages

--- MarissaCrawfordPortfolio/src/App.js
+++ MarissaCrawfordPortfolio/src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
+import Routes from './components/routes';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-<div>
-
-</div>  
+ <div style={{height: '300px', position: 'relative'}}>
+    <Layout>
+        <Header transparent title="Marissa Crawford" style={{color: 'white'}}>
+            <Navigation>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/portfolio">Portfolio</Link>
+                <Link to="/resume">Resume</Link>
+                <Link to="/contactpage">Contact Page</Link>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+            <Link to="/aboutme">About Me</Link>
+                <Link to="/portfolio">Portfolio</Link>
+                <Link to="/resume">Resume</Link>
+                <Link to="/contactpage">Contact Page</Link>
+            </Navigation>
+        </Drawer>
+        <Content />
+        <div className="page-content"/>
+        <Routes/>
+    </Layout>
+</div>
   );
 }
 
---
+++ MarissaCrawfordPortfolio/src/components/mainpage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MainPage   extends Component {
+    render() {
+        return(
+            <div style={{width: '100%', margin: 'auto'}}>
+
+            </div>
+        )
+    }
+}
+
+export default MainPage;
